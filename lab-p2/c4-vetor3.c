20b874e44327d027034d95c1abd7993e583f75c1
Danielle Pinheiro <168576403+sdaniverse@users.noreply.github.com>
2025-10-26 17:14:02 -0300

    Solve C4 problem

--- lab-p2/c4-vetor3.c
+++ lab-p2/c4-vetor3.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &n);
 
     int vetor[n], par = 0, im = 0;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         scanf("%d", &vetor[i]);
 
         if (vetor[i] % 2 == 0)
@@ -14,28 +14,29 @@ int main() {
             im++;
     }
 
-    int pares[par], impares[im];
     for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            if (vetor[j] % 2 == 0) {
-                pares[i] = vetor[j];
-                break;
-            }
-            else {
-                impares[i] = vetor[j];
-                break;
-            }
+        if (vetor[i] % 2 == 0) {
+            printf("%d", vetor[i]);
+            par--;
+
+            if (par != 0)
+                printf(" ");
+            else
+                printf("\n");
         }
     }
 
-    for (int i = 0; i < par; i++) {
-        printf("%d ", pares[i]);
+    for (int i = 0; i < n; i++) {
+        if (vetor[i] % 2 != 0) {
+            printf("%d", vetor[i]);
+            im--;
+
+            if (im != 0)
+                printf(" ");
+            else
+                printf("\n");
+        }
     }
-    printf("\n");
-    for (int i = 0; i < im; i++) {
-        printf("%d", impares[i]);
-    }
-    printf("\n");
 
     return 0;
 }
