7e2d2c11451a206db93049430cea27af51ed88cc
Danielle Pinheiro <168576403+sdaniverse@users.noreply.github.com>
2025-12-04 08:41:40 -0300

    Solve EX22

---
+++ substitutiva/ex22-musica.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+struct tipoMusica {
+    char nome[80];
+    int ano;
+};
+
+struct tipoBanda {
+    char nome[80];
+    int qtd;
+    struct tipoMusica musicas[100];
+};
+
+void pesquisarNomeMusica(char pesquisa[80], struct tipoBanda bandas[50], int n) {
+    int existe = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < bandas[i].qtd; j++) {
+            if (!strcmp(pesquisa, bandas[i].musicas[j].nome)) {
+                printf("%s : ano %d\n", bandas[i].nome, bandas[i].musicas[j].ano);
+                existe = 1;
+            }
+        }
+    }
+
+    if (!existe)
+        printf("Musica nao cadastrada\n");
+}
